ae1a24c28511297b61eee9f80593759a062299a4
Code_xxmyyds <xxmyyds@163.com>
2022-07-25 21:28:31 +0800

    scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,7 +1,7 @@
 class ReactiveEffect {
   private _fn: any
 
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
 
@@ -33,14 +33,18 @@ export function trigger(target, key) {
   let dep = depsMap.get(key)
 
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
 
 let activeEffect
 
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
 
   _effect.run()
 
