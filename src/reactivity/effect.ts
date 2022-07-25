34f5e53aa1b1ee3a1d8b32f1406642436eabff99
Code_xxmyyds <xxmyyds@163.com>
2022-07-25 22:04:44 +0800

    stop onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,35 @@
+import { extend } from './shared/index'
 class ReactiveEffect {
   private _fn: any
-
-  constructor(fn, public scheduler?) {
+  deps = []
+  active = true
+  onStop?: () => void
+  public scheduler: Function | undefined
+  constructor(fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
 
   run() {
     activeEffect = this
     return this._fn()
   }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 const targetMap = new Map()
@@ -25,7 +46,10 @@ export function track(target, key) {
     depsMap.set(key, dep)
   }
 
+  if (!activeEffect) return
+
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -45,8 +69,17 @@ let activeEffect
 
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler)
+  extend(_effect, options)
 
   _effect.run()
 
-  return _effect.run.bind(_effect)
+  const runner: any = _effect.run.bind(_effect)
+
+  runner.effect = _effect
+
+  return runner
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
