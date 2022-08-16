2cfc5bd58e831ad4244cfcbf826e7b908f3d2a11
Code_xxmyyds <xxmyyds@163.com>
2022-08-16 23:13:36 +0800

    component 初始化

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,34 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  }
+
+  return component
+}
+
+export function setupComponent(instance) {
+  setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent(instance: any) {
+  const component = instance.type
+  const { setup } = component
+  if (setup) {
+    const setupResult = setup()
+    handleSetupResult(instance, setupResult)
+  }
+}
+function handleSetupResult(instance, setupResult: any) {
+  if (typeof setupResult === 'object') {
+    instance.setupResult = setupResult
+  }
+
+  finishComponentSetup(instance)
+}
+function finishComponentSetup(instance: any) {
+  const component = instance.type
+  if (component.render) {
+    instance.render = component.render
+  }
+}
