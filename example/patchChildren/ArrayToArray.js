aa1c77c4ec9115eca6d11d497af2f4b2b340cf31
Code_xxmyyds <xxmyyds@163.com>
2022-09-13 23:23:35 +0800

    更新element children 双端对比2

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -39,13 +39,13 @@ import { h, ref } from '../../lib/my-mini-vue.esm.js'
 
 //a b
 //d c a b
-const prevChildren = [h('p', { key: 'A' }, 'A'), h('p', { key: 'B' }, 'B')]
-const nextChildren = [
-  h('p', { key: 'D' }, 'D'),
-  h('p', { key: 'C' }, 'C'),
-  h('p', { key: 'A' }, 'A'),
-  h('p', { key: 'B' }, 'B'),
-]
+// const prevChildren = [h('p', { key: 'A' }, 'A'), h('p', { key: 'B' }, 'B')]
+// const nextChildren = [
+//   h('p', { key: 'D' }, 'D'),
+//   h('p', { key: 'C' }, 'C'),
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+// ]
 
 //a b c
 //a b
@@ -64,6 +64,26 @@ const nextChildren = [
 //   h('p', { key: 'C' }, 'C'),
 // ]
 // const nextChildren = [h('p', { key: 'B' }, 'B'), h('p', { key: 'C' }, 'C')]
+
+//a b c d f g
+//a b e c f g
+
+const prevChildren = [
+  h('p', { key: 'A' }, 'A'),
+  h('p', { key: 'B' }, 'B'),
+  h('p', { key: 'C', id: 'c-prev' }, 'C'),
+  h('p', { key: 'D' }, 'D'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G'),
+]
+const nextChildren = [
+  h('p', { key: 'A' }, 'A'),
+  h('p', { key: 'B' }, 'B'),
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'C', id: 'c-next' }, 'C'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G'),
+]
 
 export default {
   name: 'ArrayToArray',
