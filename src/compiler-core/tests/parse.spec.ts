8ffaeb590d63fe4eaabd3677b40566f713b868a8
Code_xxmyyds <xxmyyds@163.com>
2022-09-28 22:56:46 +0800

    match element

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -13,4 +13,13 @@ describe('parse', () => {
       },
     })
   })
+
+  test('simple element div', () => {
+    const ast = baseParse('<div></div>')
+
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+    })
+  })
 })
