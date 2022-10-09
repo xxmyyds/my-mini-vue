100e2dfcedfb94d3d9a57645b046315b23b612fe
Code_xxmyyds <xxmyyds@163.com>
2022-10-09 21:31:29 +0800

    parse all

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -20,6 +20,7 @@ describe('parse', () => {
     expect(ast.children[0]).toStrictEqual({
       type: NodeTypes.ELEMENT,
       tag: 'div',
+      children: [],
     })
   })
 
@@ -30,5 +31,59 @@ describe('parse', () => {
       type: NodeTypes.TEXT,
       content: 'xxmyyds',
     })
+  })
+
+  test('hello world', () => {
+    const ast = baseParse('<div>hi,{{message}}</div>')
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: 'hi,',
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
+    })
+  })
+
+  test('nested element', () => {
+    const ast = baseParse('<div><p>hi,</p>{{message}}</div>')
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.ELEMENT,
+          tag: 'p',
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: 'hi,',
+            },
+          ],
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
+    })
+  })
+
+  test('should throw error when lack end tag', () => {
+    expect(() => {
+      baseParse('<div><span></div>')
+    }).toThrow('缺少结束标签span')
   })
 })
