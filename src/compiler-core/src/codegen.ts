334984fca6df191a280915fbb11955b3da9d5980
Code_xxmyyds <xxmyyds@163.com>
2022-10-13 22:29:26 +0800

    feat: parse interpolation

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -1,8 +1,11 @@
+import { helperMapName, TO_DISPLAY_STRING } from './runtimeHelpers'
+import { NodeTypes } from './ast'
+
 export function generate(ast) {
   const context = createCodegenContext()
   const { push } = context
 
-  push('return ')
+  genFunctionPreamble(ast, context)
 
   const functionName = 'render'
   const args = ['_ctx', '_cache']
@@ -16,17 +19,58 @@ export function generate(ast) {
     code: context.code,
   }
 }
+function genFunctionPreamble(ast: any, context: any) {
+  const { push } = context
+  const VueBinging = 'Vue'
+  const aliasHelper = (s) => `${helperMapName[s]}: _${helperMapName[s]}`
+  if (ast.helpers.length > 0) {
+    push(`const { ${ast.helpers.map(aliasHelper).join(', ')} } = ${VueBinging}`)
+  }
+  push('\n')
+  push('return ')
+}
+
 function genNode(node: any, context: any) {
+  switch (node.type) {
+    case NodeTypes.TEXT:
+      genText(context, node)
+      break
+    case NodeTypes.INTERPOLATION:
+      genInterpolation(context, node)
+      break
+    case NodeTypes.SIMPLE_EXPRESSION:
+      genExpression(context, node)
+      break
+    default:
+      break
+  }
+}
+function genText(context: any, node: any) {
   const { push } = context
   push(`'${node.content}'`)
 }
+
+function genInterpolation(context: any, node: any) {
+  const { push, helper } = context
+  push(`${helper(TO_DISPLAY_STRING)}(`)
+  genNode(node.content, context)
+  push(')')
+}
+
 function createCodegenContext() {
   const context = {
     code: '',
     push(source) {
       context.code += source
     },
+    helper(key) {
+      return `_${helperMapName[key]}`
+    },
   }
 
   return context
+}
+function genExpression(context: any, node: any) {
+  const { push } = context
+  push(`${node.content}`)
 }
--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,9 +1,13 @@
+import { NodeTypes } from './ast'
+import { TO_DISPLAY_STRING } from './runtimeHelpers'
+
 export function transform(root, options = {}) {
   const context = createTransformContext(root, options)
   // 深度优先遍历
   traverseNode(root, context)
 
   createRootCodegen(root)
+  root.helpers = [...context.helpers.keys()]
 }
 function traverseNode(node: any, context: any) {
   const nodeTransforms = context.nodeTransforms
@@ -11,15 +15,25 @@ function traverseNode(node: any, context: any) {
     const transform = nodeTransforms[i]
     transform(node)
   }
-  traverseChildren(node, context)
+
+  switch (node.type) {
+    case NodeTypes.INTERPOLATION:
+      context.helper(TO_DISPLAY_STRING)
+      break
+    case NodeTypes.ROOT:
+    case NodeTypes.ELEMENT:
+      traverseChildren(node, context)
+      break
+    default:
+      break
+  }
 }
 function traverseChildren(node: any, context: any) {
   const children = node.children
-  if (children) {
-    for (let i = 0; i < children.length; i++) {
-      const node = children[i]
-      traverseNode(node, context)
-    }
+
+  for (let i = 0; i < children.length; i++) {
+    const node = children[i]
+    traverseNode(node, context)
   }
 }
 
@@ -27,6 +41,10 @@ function createTransformContext(root: any, options: any) {
   const context = {
     root,
     nodeTransforms: options.nodeTransforms || [],
+    helpers: new Map(),
+    helper(key) {
+      context.helpers.set(key, 1)
+    },
   }
 
   return context
